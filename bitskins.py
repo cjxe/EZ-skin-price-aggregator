fa5c99f5c6f9184c25c85498b6bc809aad83b53d
Baran <scgc47@durham.ac.uk>
2021-04-18 00:46:39 +0100

    Turned 'steam + bitskins' into functions and can be run from main.py. Also constructs dataframes for both but its 2 am

--- bitskins.py
+++ bitskins.py
@@ -1,57 +1,58 @@
 import requests, urllib.parse
 from lxml import etree, html
 import json
-from pyxdameraulevenshtein import damerau_levenshtein_distance_seqs, normalized_damerau_levenshtein_distance_seqs
 import ast
 
 import lookups
 
+def search_at_bitskins():
 
-url = "https://bitskins.com/?"
-usr_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
-headers = {'user-agent': usr_agent}
+    url = "https://bitskins.com/?"
+    usr_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
+    headers = {'user-agent': usr_agent}
 
-# Pull the user input
-with open('./data/search_query.txt', 'r') as f:
-    usr_inp = f.read()
+    # Pull the user input
+    with open('./data/search_query.txt', 'r') as f:
+        usr_inp = f.read()
 
-bitskins = lookups.Bitskins()
-# Determine if `Stattrak` or `Souvenir` or neither
-usr_inp, is_no, is_st, is_so = bitskins.category(usr_inp)
+    bitskins = lookups.Bitskins()
+    # Determine if `Stattrak` or `Souvenir` or neither
+    usr_inp, is_no, is_st, is_so = bitskins.category(usr_inp)
 
-# Setting weapon exterior (condition, wear)
-usr_inp, weapon_exterior = bitskins.exterior(usr_inp)
+    # Setting weapon exterior (condition, wear)
+    usr_inp, weapon_exterior = bitskins.exterior(usr_inp)
 
-# Search for weapon
-usr_inp, weapon = lookups.we_name(usr_inp)
+    # Search for weapon
+    usr_inp, weapon = lookups.we_name(usr_inp)
 
-# Search for skin
-skin = lookups.we_skin(usr_inp)
+    # Search for skin
+    skin = lookups.we_skin(usr_inp)
 
-data = { 'market_hash_name': skin,
-         'advanced': '1',
-         'appid':'730',
-         'item_wear': weapon_exterior,
-         'is_stattrak': is_st,
-         'is_souvenir': is_so,
-         'item_weapon': weapon,
-         'sort_by': 'price',
-         'order': 'asc'
-}
+    data = { 'market_hash_name': skin,
+            'advanced': '1',
+            'appid':'730',
+            'item_wear': weapon_exterior,
+            'is_stattrak': is_st,
+            'is_souvenir': is_so,
+            'item_weapon': weapon,
+            'sort_by': 'price',
+            'order': 'asc'
+    }
 
-encoded = urllib.parse.urlencode(query=data)
-print(url + encoded)
+    encoded = urllib.parse.urlencode(query=data)
+    # print(url + encoded)
 
-r = requests.get(url + encoded, headers=headers)
-# print(r.content)
+    r = requests.get(url + encoded, headers=headers)
+    # print(r.content)
 
-doc = html.fromstring(r.content)
-tree = etree.ElementTree(doc)
+    doc = html.fromstring(r.content)
+    tree = etree.ElementTree(doc)
 
-price = tree.xpath('//span[@class="item-price-display"]')
-price = price[0].text_content()
-price = float(price[1:])
-sell_now_price = round(price * 0.95,2)
+    price = tree.xpath('//span[@class="item-price-display"]')
+    price = price[0].text_content()
+    price = float(price[1:])
+    sell_now_price = round(price * 0.95,2)
 
-print(["BitSkins", weapon, skin, weapon_exterior, 0, price, sell_now_price])
+    # print(["BitSkins", weapon, skin, weapon_exterior, 0, price, sell_now_price])
+    return ["BitSkins", weapon, skin, weapon_exterior, 0, price, sell_now_price]
 
--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import pandas as pd
+import bitskins, steam_market
 
+usr_inp = input('Search skin: ')
+
+with open('./data/search_query.txt','w') as f:
+    f.write(usr_inp)
+
+df = pd.DataFrame(columns=['market', 'weapon', 'skin', 'wear', 'float', 'buy', 'sell'])
+df.to_csv('./data/search_result.csv', mode='w', index=False)
+
+
+row_1 = steam_market.search_at_steam()  # fix these
+new_row = pd.DataFrame([row_1])
+new_row.to_csv('./data/search_result.csv', index=False, mode='a', header=False)
+
+row_2 = bitskins.search_at_bitskins()
+new_row = pd.DataFrame([row_2])
+new_row.to_csv('./data/search_result.csv', index=False, mode='a', header=False)
+
+
+df = pd.read_csv('./data/search_result.csv')  # debug
+print(df)  # debug
+
+
+
+"""
 search_q = 'awp asiimof ft'
 
 filename = 'search_result.csv'
@@ -17,3 +42,9 @@ dfa = pd.DataFrame([["Bynogame", "AWP | Asiimov (Field-Tested)", "0.2687", "$69.
                      columns=["Market", "Weapon | Skin (Condition)", "Float", "Price", "Native", "Discount"])
 df = df.append(dfa, ignore_index=True)
 print(df)
+
+
+# Native'i sil
+# Satin al vs Sat   ekle, sat'ta komisyon dahil olsun
+# discount en pahali vs en ucuz site olsun
+"""
